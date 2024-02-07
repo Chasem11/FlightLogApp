019a2671d02723b6227e8e47721d655e23be12c8
Chase Moffat <chase@MacBook-Pro.local>
2024-02-06 19:30:56 -0500

    first commit

---
+++ FlightLogApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlightLogApp
+//
+//  Created by Chase Moffat on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlightLogApp/FlightLogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightLogAppApp.swift
+//  FlightLogApp
+//
+//  Created by Chase Moffat on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FlightLogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
